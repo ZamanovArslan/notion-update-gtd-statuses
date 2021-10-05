2e34bf6a135c66f28450013673c01091cefbb04b
Arslan Zamanov <arszamanov@gmail.com>
2021-10-06 01:07:51 +0300

    add parallel requests

--- notion_update_gtd_statuses.rb
+++ notion_update_gtd_statuses.rb
@@ -5,6 +5,7 @@ require_relative "components/set_icons"
 require_relative "components/set_projects_progress"
 require_relative "lib/updates_optimizer"
 require 'byebug'
+require 'thread'  # for Mutex
 
 class NotionUpdateGtdStatuses
   extend Dry::Configurable
@@ -15,19 +16,45 @@ class NotionUpdateGtdStatuses
   def self.run
     progressbar = ProgressBar.create(title: "Updating pages")
 
-    pages = config.components.sum([]) do |component|
-      component.new(config.notion_client).get_pages
-    end
-
-    pages = UpdatesOptimizer.new(pages).get_pages
+    pages = UpdatesOptimizer.new(recieve_pages_parallel).get_pages
 
     progressbar.total = pages.size
 
-    pages.each do |page|
-      progressbar.increment
-      sleep 0.01
+    update_pages_parallel(pages, progressbar)
+  end
 
-      config.notion_client.update_page(id: page.id, **page.new_props)
-    end
+  def self.update_pages_parallel(pages, progressbar)
+    mutex = Mutex.new
+
+    8.times.map do
+      Thread.new(pages) do |pages|
+        while page = mutex.synchronize { pages.pop }
+          config.notion_client.update_page(id: page.id, **page.new_props)
+
+          mutex.synchronize do
+            progressbar.increment
+          end
+        end
+      end
+    end.each(&:join)
+  end
+
+  def self.recieve_pages_parallel
+    mutex = Mutex.new
+    pages = []
+
+    8.times.map do
+      Thread.new(config.components, pages) do |components, pages|
+        while component = mutex.synchronize { components.pop }
+          new_pages = component.new(config.notion_client).get_pages
+
+          mutex.synchronize do
+            pages.concat new_pages
+          end
+        end
+      end
+    end.each(&:join)
+
+    pages
   end
 end
